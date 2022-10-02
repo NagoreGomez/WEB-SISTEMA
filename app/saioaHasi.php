9d748694a03142c42c89546e2dd108b4f838e213
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-10-02 20:43:21 +0200

    Update saioaHasi.php

--- app/saioaHasi.php
+++ app/saioaHasi.php
@@ -1,27 +1,12 @@
 <?php
-#require "index.php";
 
-#Session_start();
+require "index.php";
 
+session_start();
 
+    echo '<h1>Yeah, aa!<h1>';
  
-    echo '<h1>Yeah, it worksaaaa!<h1>';
- 
- $query = mysqli_query($conn, "SELECT * FROM usuarios")
-   or die (mysqli_error($conn));
 
-while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    <td>{$row['NAN']}</td>
-    <td>{$row['Izen abizenak']}</td>
-    <td>{$row['Telefonoa']}</td>
-    <td>{$row['Jaiotze data']}</td>
-    <td>{$row['Email']}</td>
-   </tr>";
-
-
-}
 
 
 ?>
