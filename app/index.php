cfd71552d732221fc005c54ff03dde89cfe52374
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-10-02 20:43:02 +0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,21 +1,32 @@
 <?php
-  // phpinfo();
-  $hostname = "db";
-  $username = "admin";
-  $password = "test";
-  $db = "database";
+ 
 
-  $conn = mysqli_connect($hostname,$username,$password,$db);
-  if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-  }
+require "konexioa.php";
 
-
-  header("Location: http://localhost:81/saioaHasi.php");
-  exit;    
-
-
-
+session_start();
 
  
+
+$query = mysqli_query($conn, "SELECT * FROM usuarios")
+   or die (mysqli_error($conn));
+
+while ($row = mysqli_fetch_array($query)) {
+  echo
+   "<tr>
+    <td>{$row['NAN']}</td>
+    <td>{$row['Izen abizenak']}</td>
+    <td>{$row['Telefonoa']}</td>
+    <td>{$row['Jaiotze data']}</td>
+    <td>{$row['Email']}</td>
+   </tr>";
+   
+
+}
+
+  #header("Location: http://localhost:81/saioaHasi.php");
+  #exit;
+  
+   
+
+
 ?>
