03b4574a779d60ea7ec958dee33799570e931243
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-10-02 18:57:37 +0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,5 +1,4 @@
 <?php
-  echo '<h1>Yeah, it works!<h1>';
   // phpinfo();
   $hostname = "db";
   $username = "admin";
@@ -11,28 +10,5 @@
     die("Database connection failed: " . $conn->connect_error);
   }
 
- header("Location: http://localhost:81/saioaHasi.php");
-
-
-$query = mysqli_query($conn, "SELECT * FROM usuarios")
-   or die (mysqli_error($conn));
-
-while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    <td>{$row['NAN']}</td>
-    <td>{$row['Izen abizenak']}</td>
-    <td>{$row['Telefonoa']}</td>
-    <td>{$row['Jaiotze data']}</td>
-    <td>{$row['Email']}</td>
-   </tr>";
-   
-
-}
-
-echo '<h1>Yeah, it works!<h1>';
-
-
-
-
+ 
 ?>
