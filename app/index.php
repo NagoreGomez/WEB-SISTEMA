c8319ae6a9dd566978c3b91ca9db992b86916538
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-10-02 18:50:53 +0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -11,6 +11,7 @@
     die("Database connection failed: " . $conn->connect_error);
   }
 
+ header("Location: http://localhost:81/saioaHasi.php");
 
 
 $query = mysqli_query($conn, "SELECT * FROM usuarios")
