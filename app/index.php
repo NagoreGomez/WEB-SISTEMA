5a26e88b16447ce70467a63e6bf74af7985acac3
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-10-02 17:55:08 +0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -29,4 +29,20 @@ while ($row = mysqli_fetch_array($query)) {
 
 }
 
+  $izena = $_POST['Izena'];
+  $pasahitza = $_POST['Pasahitza'];
+  
+  $sql= "SELECT * FROM 'usuarios' WHERE 'Izena'='$izena' and 'Pasahitza'='$pasahitza'";
+  $query = mysqli_qery($conn,$sql);
+  $row = mysqli_fetch_array($query);
+
+    if($row['Izena']!=null){ 
+      $_SESSION['Izena'] = $izena;     
+      header("Location: http://localhost:81/saioaHasi.php");
+      exit;      
+   
+   }
+
+
+
 ?>
