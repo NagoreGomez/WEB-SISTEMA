0e3ebe76675381ea157124bd26fc27c987fdea08
jonasmartinnez <82470464+jonasmartinnez@users.noreply.github.com>
2022-10-02 18:37:25 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -12,34 +12,34 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT/;
-/!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS/;
-/!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION/;
-/!40101 SET NAMES utf8mb4/;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: database
+-- Base de datos: `database`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla usuarios
+-- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE usuarios (
-  NAN varchar(9) NOT NULL,
-  Izen abizenak text NOT NULL,
-  Telefonoa int(9) NOT NULL,
-  Jaiotze data date NOT NULL,
-  Email text NOT NULL
+CREATE TABLE `usuarios` (
+  `NAN` varchar(9) NOT NULL,
+  `Izen abizenak` text NOT NULL,
+  `Telefonoa` int(9) NOT NULL,
+  `Jaiotze data` date NOT NULL,
+  `Email` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla usuarios
+-- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO usuarios (NAN, Izen abizenak,Telefonoa,Jaiotze data,Email) VALUES
+INSERT INTO `usuarios` (`NAN`, `Izen abizenak`,`Telefonoa`,`Jaiotze data`,`Email`) VALUES
 ('12345678A', 'Nagore Gomez', '698876543','2002-11-05', 'a@gmail.com'),
 ('12345678B', 'Jonas Martinez','634768598','2002-04-14', 'b@gmail.com'),
 ('12345678C', 'Sergio Martin','665289605','2002-06-24', 'c@gmail.com');
@@ -49,12 +49,12 @@ INSERT INTO usuarios (NAN, Izen abizenak,Telefonoa,Jaiotze data,Email) VALUES
 --
 
 --
--- Indices de la tabla usuarios
+-- Indices de la tabla `usuarios`
 --
-ALTER TABLE usuarios
-  ADD PRIMARY KEY (NAN);
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`NAN`);
 COMMIT;
 
-/!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT/;
-/!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS/;
-/!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION/;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
