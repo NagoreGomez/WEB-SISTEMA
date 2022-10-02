04c1546db9fa6795486a40fa213efc86bbb7fc79
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-10-02 17:55:29 +0200

    Update saioaHasi.php

--- app/saioaHasi.php
+++ app/saioaHasi.php
@@ -1,19 +1,5 @@
 <?php
-require "index.php"
-
-session_sart();
-
-  $izena = $_POST['Izena'];
-  $pasahitza = $_POST['Pasahitza'];
-  
-  $sql= "SELECT * FROM 'usuarios' WHERE 'Izena'='$izena' and 'Pasahitza'='$pasahitza'";
-  $query = mysqli_qery($conn,$sql);
-  $row = mysqli_fetch_array($query);
-
-    if($row['Izena']!=null){ 
-      $_SESSION['Izena'] = $izena;     
-      header("Location: http://localhost:81/index.php");
-      exit;      
+     echo '<h1>Yeah, it works!<h1>';
    
-   }
+ 
 ?>
