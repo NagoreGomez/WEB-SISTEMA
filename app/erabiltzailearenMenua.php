6235a5da0faa20c145ff6ce937803caa52478c0a
jonasmartinnez <82470464+jonasmartinnez@users.noreply.github.com>
2022-10-03 19:17:05 +0200

    Update erabiltzailearenMenua.php

--- app/erabiltzailearenMenua.php
+++ app/erabiltzailearenMenua.php
@@ -3,8 +3,16 @@
 require "konexioa.php";
 
 session_start();
-
-    echo '<h1>Erabiltzaile menua!!!!<h1>';
+    $nan= $_POST['NAN'];
+    $izena= $_POST['Izen abizenak']
+    $jaio= $_POST['Jaiotze data'];
+    $email= $_POST['Email'];
+    $telefonoa= $_POST['Telefonoa'];
+    $pasahitza= $_POST['Pasahitza'];
+    
+    $sql ="INSERT INTO `erabiltzaileak` VALUES ('$nan', '$izena', '$jaio', '$email', '$telefonoa', '$pasahitza')
+    $query = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_array($query);
  
 
 
