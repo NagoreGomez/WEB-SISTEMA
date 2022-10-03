463a8717370cc19baefba07bcf24be7f322039ec
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-10-03 10:51:05 +0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -7,21 +7,21 @@ session_start();
 
  
 
-$query = mysqli_query($conn, "SELECT * FROM usuarios")
-   or die (mysqli_error($conn));
+#$query = mysqli_query($conn, "SELECT * FROM usuarios")
+  # or die (mysqli_error($conn));
 
-while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    #<td>{$row['NAN']}</td>
-    <td>{$row['Izen abizenak']}</td>
-    <td>{$row['Telefonoa']}</td>
-    <td>{$row['Jaiotze data']}</td>
-    <td>{$row['Email']}</td>
-   </tr>";
+#while ($row = mysqli_fetch_array($query)) {
+  #echo
+   #"<tr>
+    ##<td>{$row['NAN']}</td>
+    #<td>{$row['Izen abizenak']}</td>
+    #<td>{$row['Telefonoa']}</td>
+   # <td>{$row['Jaiotze data']}</td>
+    #<td>{$row['Email']}</td>
+   #</tr>";
    
 
-}
+#}
 
   #header("Location: http://localhost:81/saioaHasi.php");
   #exit;
