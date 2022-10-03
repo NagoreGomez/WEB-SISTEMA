d823bb5b451d633d25c4355c711cc1dbdab3b4cd
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-10-03 10:39:04 +0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -26,7 +26,24 @@ while ($row = mysqli_fetch_array($query)) {
   #header("Location: http://localhost:81/saioaHasi.php");
   #exit;
   
-   
-
-
+  
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<form class="login-form">
+  <p class="login-text">
+    <span class="fa-stack fa-lg">
+       <i class="fa fa-user fa-solid-2x"></i>
+      
+    </span>
+  </p>
+  <input type="email" class="login-username" autofocus="true" required="true" placeholder="Email" />
+  <input type="password" class="login-password" required="true" placeholder="Password" />
+  <input type="submit" name="Login" value="Login" class="login-submit" />
+  <input type="submit" name="Erregistratu" value="Erregistratu" class="login-submit2" />
+</form>
+<a href="#" class="login-forgot-pass">forgot password?</a>
+<div class="underlay-photo"></div>
+<div class="underlay-black"></div> 
+
+
