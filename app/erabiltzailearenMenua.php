db88b36df8944773a5fa5d977ec4879938b0c10b
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-10-06 11:42:50 +0200

    Update erabiltzailearenMenua.php

--- app/erabiltzailearenMenua.php
+++ app/erabiltzailearenMenua.php
@@ -1,1 +1,38 @@
 
+<?php>
+  print ("holaaaa")
+ <?>
+
+<!DOCTYPE html>
+<html lang="es">
+ <link
+      rel="stylesheet"
+       type=text/css
+      href="./erregistratu.css"
+    />
+ 
+ 
+ <body>
+<form class="login-form" action="erregistratu.php" method="POST" >
+  <p class="login-text">
+    <span class="fa-stack fa-lg">
+       <i class="fa fa-user fa-solid-2x"></i>
+      <p style="font-size: 20px;margin: 0px 0; color:white">$izena hauek dira zure ibilbideak:</p>
+
+      
+    </span>
+  </p>
+  <p>
+  </p><input type="submit" name="Ezabatu" value="Ezabatu" class="ezabatu-gehitu-submit" />
+  <input type="submit" name="Ibilbide berria gehitu" value="Ibilbide berria gehitu" class="ezabatu-gehitu-submit" />
+  
+</form>
+<a href="index.php" class="erregistro-itzuli">Itzuli</a>
+
+
+<div class="underlay-photo"></div>
+<div class="underlay-black"></div> 
+
+
+  </body>
+ </html>
