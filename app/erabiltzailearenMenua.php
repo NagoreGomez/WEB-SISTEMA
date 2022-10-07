c58439d703126ef30da2b223f6084a58ca6a9989
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-10-07 17:07:15 +0200

    Update erabiltzailearenMenua.php

--- app/erabiltzailearenMenua.php
+++ app/erabiltzailearenMenua.php
@@ -1,38 +1,81 @@
 
 <?php
-  print ("holaaaa")
- ?>
+ 
 
+require "konexioa.php";
+
+session_start();
+
+    $email= $_POST['Email'];
+    $pasahitza= $_POST['Pasahitza'];
+    
+    
+    $sql ="SELECT * FROM `erabiltzaileak` WHERE `Email` = '$email' and `Pasahitza` = '$pasahitza'";
+    $query = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_array($query);
+
+    if($row['Email']!=null){ #erabiltzailea erregistratuta egotekotan 
+      $_SESSION['Email'] = $email;     
+      header("Location: http://localhost:81/erabiltzailearenMenua.php");
+      exit;      
+    }
+    
+    
+    
+
+ /*
+ $query = mysqli_query($conn, "SELECT * FROM erabiltzaileak WHERE `Email` = '$email'")
+  or die (mysqli_error($conn));
+
+while ($row = mysqli_fetch_array($query)) {
+  echo
+  "<tr>
+   <td>{$row['NAN']}</td>
+    <td>{$row['Izen abizenak']}</td>
+    <td>{$row['Telefonoa']}</td>
+    <td>{$row['Jaiotze data']}</td>
+   <td>{$row['Email']}</td>
+   <td>{$row['Pasahitza']}</td>
+  </tr>";
+   
+
+}
+
+*/
+
+ 
+  
+  
+?>
 <!DOCTYPE html>
 <html lang="es">
  <link
       rel="stylesheet"
        type=text/css
-      href="./erregistratu.css"
+      href="./index.css"
     />
  
  
  <body>
-<form class="login-form" action="erregistratu.php" method="POST" >
+ 
+<form class="login-form" action="index.php" method="POST" > 
+
   <p class="login-text">
     <span class="fa-stack fa-lg">
-       <i class="fa fa-user fa-solid-2x"></i>
-      <p style="font-size: 20px;margin: 0px 0; color:white">$izena hauek dira zure ibilbideak:</p>
-
+       <i class="fa fa-lock fa-solid-2x"></i>
       
     </span>
   </p>
-  <p>
-  </p><input type="submit" name="Ezabatu" value="Ezabatu" class="ezabatu-gehitu-submit" />
-  <input type="submit" name="Ibilbide berria gehitu" value="Ibilbide berria gehitu" class="ezabatu-gehitu-submit" />
+  <input type="email" name="Email" class="login-info" autofocus="true" required="true" placeholder="Helbide elektronikoa" />
+  <input type="password"  name="Pasahitza"  class="login-info" required="true" placeholder="Pasahitza" />
+  <input type="submit" name="Login" value="Login" class="login-submit" />
   
 </form>
-<a href="index.php" class="erregistro-itzuli">Itzuli</a>
+<a class="login-erregistro" href="erregistratu.php" >Erregistratu nahi duzu?</a>
 
 
 <div class="underlay-photo"></div>
 <div class="underlay-black"></div> 
-
 
   </body>
  </html>
