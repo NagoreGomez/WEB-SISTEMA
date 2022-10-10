a58b91dc35e96e90487e9043c3c058787762ad95
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-10-10 19:19:00 +0200

    Update erabiltzailearenMenua.php

--- app/erabiltzailearenMenua.php
+++ app/erabiltzailearenMenua.php
@@ -1,82 +1,102 @@
 
-<?php
- 
+<?php 
 
-require "konexioa.php";
-if( empty(session_id()) && !headers_sent()){
-    session_start();
-}
+  require "konexioa.php";
+  session_start();
+  
+  $email= $_SESSION['Email'];
+  $izena= $_SESSION['Izen abizenak'];
+  
 
-    $email= $_POST['Email'];
-    $pasahitza= $_POST['Pasahitza'];
-    
-    
-    $sql ="SELECT * FROM `erabiltzaileak` WHERE `Email` = '$email' and `Pasahitza` = '$pasahitza'";
-    $query = mysqli_query($conn,$sql);
-    $row = mysqli_fetch_array($query);
+  
+  
+  
+  $ibilbideak = mysqli_query($conn, "SELECT * FROM `ibilbideak` WHERE `Email` = '$email'")
 
-    if($row['Email']!=null){ #erabiltzailea erregistratuta egotekotan 
-      $_SESSION['Email'] = $email;     
-      header("Location: http://localhost:81/erabiltzailearenMenua.php");
-      exit;      
-    }
-    
-    
-    
-
- /*
- $query = mysqli_query($conn, "SELECT * FROM erabiltzaileak WHERE `Email` = '$email'")
-  or die (mysqli_error($conn));
-
-while ($row = mysqli_fetch_array($query)) {
+/*
+while ($row = mysqli_fetch_array($ibilbideak)) {
   echo
-  "<tr>
-   <td>{$row['NAN']}</td>
-    <td>{$row['Izen abizenak']}</td>
-    <td>{$row['Telefonoa']}</td>
-    <td>{$row['Jaiotze data']}</td>
-   <td>{$row['Email']}</td>
-   <td>{$row['Pasahitza']}</td>
-  </tr>";
+   "<tr>
+    <td>{$row['Ibilbidearen Izena']}</td>
+    <td>{$row['Zailtasuna']}</td>
+    <td>{$row['Distantzia (m)']}</td>
+    <td>{$row['Desnibela (m)']}</td>
+    <td>{$row['Link-a']}</td>
+   </tr>";
    
 
 }
 
+
 */
 
- 
+
+
   
-  
-?>
+ ?>
+
 <!DOCTYPE html>
 <html lang="es">
  <link
       rel="stylesheet"
        type=text/css
-      href="./index.css"
+      href="./erabiltzailearenMenua.css"
     />
  
  
  <body>
- 
-<form class="login-form" action="index.php" method="POST" > 
-
+<form class="login-form" action="erabiltzailearenMenua.php" method="POST" >
   <p class="login-text">
     <span class="fa-stack fa-lg">
-       <i class="fa fa-lock fa-solid-2x"></i>
-      
+       <!-- <i class="fa fa-user fa-solid-2x"></i> -->
+      <p style="font-size: 20px;margin: 0px 0; color:white">Hauek dira zure ibilbideak:</p>
+       
+       <table class="taula-erabiltzailea">
+            <thead class="thead-taula">
+              <tr>
+                <th scope="col">Ibilbidearen izena</th>
+                <th scope="col">Zailtasuna</th>
+                <th scope="col">Distantzia (m)</th>
+                <th scope="col">Desnibela (m)</th>
+                <th scope="col">Link-a</th>
+                <th scope="col">Aldatu</th>  <!-- ????-->
+                <th scope="col">Ezabatu</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php      
+              while($row=mysqli_fetch_array($ibilbideak)){
+                echo
+                "<tr>
+                  <td>{$row['Ibilbidearen Izena']}</td>
+                  <td>{$row['Zailtasuna']}</td>         
+                  <td>{$row['Distantzia (m)']}</td>
+                  <td>{$row['Desnibela (m)']}</td>
+                  <td><a href='{$row['Link-a']}' class='badge badge-info' target='blank'>Link-a</a></td> <!-- investigarr  -->
+                 <!-- INVESTIGARRR <td><a href='apunteaAldatu.php?id={$row['Id']}' class='badge badge-warning'>editatu</a></td>  -->
+                  <td><button id='btnModal' type='button' class='badge badge-danger' data-id='{$row['Id']}' data-toggle='modal' data-target='#modalEzabatu'>Aldatu</button></td>
+                  <td><button id='btnModal' type='button' class='badge badge-danger' data-id='{$row['Id']}' data-toggle='modal' data-target='#modalEzabatu'>Ezabatu</button></td>
+                </tr>";
+              }
+              ?>
+            </tbody>
+          </table> 
+       
+       
+       
     </span>
   </p>
-  <input type="email" name="Email" class="login-info" autofocus="true" required="true" placeholder="Helbide elektronikoa" />
-  <input type="password"  name="Pasahitza"  class="login-info" required="true" placeholder="Pasahitza" />
-  <input type="submit" name="Login" value="Login" class="login-submit" />
+  <p>
+  </p><input type="submit" name="Ezabatu" value="Ezabatu" class="ezabatu-gehitu-submit" />
+  <input type="submit" name="Ibilbide berria gehitu" value="Ibilbide berria gehitu" class="ezabatu-gehitu-submit" />
   
 </form>
-<a class="login-erregistro" href="erregistratu.php" >Erregistratu nahi duzu?</a>
+<a href="index.php" class="erregistro-itzuli">Itzuli</a>
 
 
 <div class="underlay-photo"></div>
 <div class="underlay-black"></div> 
+
 
   </body>
  </html>
