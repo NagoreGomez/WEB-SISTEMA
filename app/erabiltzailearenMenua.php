2934378b6dcd018af154cf36579c2ecc94e9b1e3
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-10-11 17:39:09 +0200

    Add files via upload

---
+++ app/erabiltzailearenMenua.php
@@ -0,0 +1,82 @@
+<?php 
+
+  require "konexioa.php";
+  session_start();
+  
+  $email= $_SESSION['Email'];
+  $izena= $_SESSION['Izen abizenak'];
+ 
+  
+    
+  $ibilbideak = mysqli_query($conn, "SELECT * FROM `ibilbideak` WHERE `Email` = '$email'")
+
+  
+ ?>
+
+<!DOCTYPE html>
+<html lang="es">
+ <link
+      rel="stylesheet"
+       type=text/css
+      href="./erabiltzailearenMenua.css"
+    />
+ 
+ 
+ <body>
+<form class="login-form" action="index.php" method="POST" >        <!-- HAU ALDATU-->
+  <p class="login-text">
+    <span class="fa-stack fa-lg">
+       <!-- <i class="fa fa-user fa-solid-2x"></i> -->
+      <p style="font-size: 20px;margin: 0px 0; color:white">Hauek dira zure ibilbidea:</p>
+       
+        <table class="taula-erabiltzailea">
+            <thead class="thead-taula">
+              <tr>
+                <th scope="col"<td style="border: solid 3px white ;"><font color="white">Ibilbidearen izena</th>
+                <th scope="col"<td style="border: solid 3px white ;"><font color="white">Zailtasuna</th>
+                <th scope="col"<td style="border: solid 3px white ;"><font color="white">Distantzia (m)</th>
+                <th scope="col"<td style="border: solid 3px white ;"><font color="white">Desnibela (m)</th>
+                <th scope="col"<td style="border: solid 3px white ;"><font color="white">Ibilbidearen link-a</th>
+                <th scope="col"<td style="border: solid 3px white ;"><font color="white">Aldatu</th>  <!-- ????-->
+                <th scope="col"<td style="border: solid 3px white ;"><font color="white">Ezabatu</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php  
+               
+              while($row=mysqli_fetch_array($ibilbideak)){
+                echo
+                "<tr>
+                  <td class=letra>{$row['Ibilbidearen izena']}</td>
+                  <td class=letra>{$row['Zailtasuna']}</td>         
+                  <td class=letra>{$row['Distantzia (m)']}</td>
+                  <td class=letra>{$row['Desnibela (m)']}</td>
+                 <td class=letra><button id='btnModal' type='button'><a href='{$row['Link-a']}'>Link-a</button></td>
+                 <!-- INVESTIGARRR <td><a href='apunteaAldatu.php?id={$row['Id']}' class='badge badge-warning'>editatu</a></td>  -->
+                  <td><a href='ibilbideaAldatu.php?id={$row['Ibilbidearen izena']}' >Aldatu</a></td>
+                  <td class=letra><button id='btnModal' type='button' class='badge badge-danger' data-id='{$row['Id']}' data-toggle='modal' data-target='#modalEzabatu'><a href=index.php> Ezabatu</button></td>
+                </tr>";
+                
+              }
+              ?> <!-- HAU ALDATU, BOTOIEN LINKA-->
+            </tbody>
+          </table> 
+       
+       
+       
+    </span>
+  </p>
+  <p>
+  </p>
+  <input type="submit" name="Ibilbide berria gehitu" value="Ibilbide berria gehitu" class="ezabatu-gehitu-submit" /> 
+  
+</form>
+<a href="index.php" class="erregistro-itzuli">Itzuli</a>   
+
+
+<div class="underlay-photo"></div>
+<div class="underlay-black"></div> 
+
+
+  </body>
+ </html>
---
+++ app/ibilbideAldaketaEgin.php
@@ -0,0 +1,30 @@
+<?php 
+
+  require "konexioa.php";
+  
+  session_start();
+  
+  $email= $_SESSION['Email'];
+  $izena= $_SESSION['Izen abizenak'];
+ 
+  
+  $ibilbidea=$_POST['Izena'];
+  $zailtasuna= $_POST['Zailtasuna'];
+  $distantzia= $_POST['Distantzia'];
+  $desnibela= $_POST['Desnibela'];
+  $link= $_POST['Link-a'];
+   
+$sql="UPDATE `ibilbideak` SET `Ibilbidearen izena`='$ibilbidea',`Zailtasuna` = '$zailtasuna', `Distantzia (m)`='$distantzia', `Desnibela (m)`='$desnibela',`Link-a`='$link' WHERE `Ibilbidearen izena`='$ibilbidea' AND `Email` = '$email' ";
+
+ echo "$sql";
+$query = mysqli_query($conn, $sql);
+
+if($query){
+    header("Location: http://localhost:81/ibilbideaAldatu.php");   //esto es lo k no va bien
+    exit;
+}
+ 
+ ?>
+
+
+ 
