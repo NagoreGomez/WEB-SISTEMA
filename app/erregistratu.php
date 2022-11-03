42438656de01f5dc0249e693616338085d77afe9
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-11-03 13:39:44 +0100

    Update erregistratu.php

--- app/erregistratu.php
+++ app/erregistratu.php
@@ -5,29 +5,54 @@ require "konexioa.php";
 
 session_start();
 
-if($conn){
-    $nan= $_POST['NAN'];
-    $izena= $_POST['Izena'];
-    $jaio= $_POST['Jaiotza'];
-    $email= $_POST['Email'];
-    $telefonoa= $_POST['Telefonoa'];
-    $pasahitza= $_POST['Pasahitza1'];
-  
-  
-    $sql="INSERT INTO `erabiltzaileak` (`NAN`, `Izen abizenak`,`Telefonoa`,`Jaiotze data`,`Email`,`Pasahitza` ) VALUES ('$nan', '$izena', '$telefonoa','$jaio', '$email', '$pasahitza')";
-    $query = mysqli_query($conn, $sql);
-     
-    
-     if($query){
-        header("Location: http://localhost:81/index.php");
-        exit;
-      }
 
+
+if (empty($_SESSION['token'])) {
+  $_SESSION['token'] = bin2hex(random_bytes(32));
 }
+
+
+$token = $_SESSION['token'];
+
+
+
+if (!empty($_POST['csrf'])) {
+  if (hash_equals($_SESSION['token'], $_POST['csrf'])) {
+    // Procesar el formulario
+    if($conn){
+    	$nan= $_POST['NAN'];
+	$izena= $_POST['Izena'];
+	$jaio= $_POST['Jaiotza'];
+	$email= $_POST['Email'];
+	$telefonoa= $_POST['Telefonoa'];
+	$pasahitza= $_POST['Pasahitza1'];
+
+
+	$sql="INSERT INTO `erabiltzaileak` (`NAN`, `Izen abizenak`,`Telefonoa`,`Jaiotze data`,`Email`,`Pasahitza` ) VALUES ('$nan', '$izena', '$telefonoa','$jaio', '$email', '$pasahitza')";
+	$query = mysqli_query($conn, $sql);
+
+
+	if($query){
+		header("Location: http://localhost:81/index.php");
+		exit;
+	}
+    }
+    
+
+
+  } else {
+    // Posible petición malintencionada
+    // Se recomienda guardar este acceso en un log
+    echo("hacer algooooo");
+  }
+}
+
+
 
 header( 'X-Content-Type-Options: nosniff' );
 header( 'X-Frame-Options: SAMEORIGIN' );
 header( 'X-XSS-Protection: 1;mode=block' );
+
 
 
 ?>
@@ -42,8 +67,11 @@ header( 'X-XSS-Protection: 1;mode=block' );
        type=text/css
       href="./erregistratu.css"
     />
+ 
+ 
  <body>
 <form class="login-form" action="erregistratu.php" method="POST" id="erregistroa">
+<input name="csrf" type="hidden" value="<?php echo $_SESSION['token']; ?>">
   <p class="login-text" >
   <img src="../irudiak/mendia.png"  width="90" >
   </p>
@@ -56,6 +84,9 @@ header( 'X-XSS-Protection: 1;mode=block' );
    <input type="password"  name="Pasahitza2" id="Pasahitza2" class="login-info" required="true" placeholder="Pasahitza errepikatu" />
    
   <input value="Erregistratu" class="erregistratu-submit" type="submit" />
+  
+  
+  
 
   
 <script src="../js/erregistratu.js"></script>
