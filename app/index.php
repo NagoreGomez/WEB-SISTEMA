f9714030e8e262c5eef07697738f82214b1f8ed1
Sergio Martín <81020872+Sergiom8m@users.noreply.github.com>
2022-11-03 10:57:38 +0100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -21,7 +21,14 @@ session_start();
       exit;      
     }
     
-    
+
+
+
+    header( 'X-Content-Type-Options: nosniff' );
+    header( 'X-Frame-Options: SAMEORIGIN' );
+    header( 'X-XSS-Protection: 1;mode=block' );
+
+
     
 
  /*
