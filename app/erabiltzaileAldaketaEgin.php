7470eb5bb7bddb32eac05a7c8ed04e6a5c2850e2
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-11-15 17:32:31 +0100

    Update erabiltzaileAldaketaEgin.php

--- app/erabiltzaileAldaketaEgin.php
+++ app/erabiltzaileAldaketaEgin.php
@@ -4,6 +4,9 @@
   
   session_start();
   
+  header( 'X-Content-Type-Options: nosniff' );
+  header( 'X-Frame-Options: SAMEORIGIN' );
+  header( 'X-XSS-Protection: 1;mode=block' );
   header_remove('X-Powered-By');
   
   $email= $_SESSION['Email'];
@@ -35,13 +38,16 @@
 		header("Location: http://localhost:81/erabiltzailearenMenua.php"); 
 		exit;
 	}
-  
   }  
-  	
-  
- 
+  	 
 ?>
- 
+ <html lang="es">
+
+<header>
+<meta http-equiv="Content-Security-Policy" content="default-src 'self' ;" >
+</header>
+
+</html>
  
 
 
