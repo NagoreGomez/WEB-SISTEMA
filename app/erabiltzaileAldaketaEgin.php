3ec737ecf7f94a3bbccaeb09010dc8211b3c0afb
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2022-11-19 18:34:44 +0100

    Add files via upload

---
+++ app/erabiltzaileAldaketaEgin.php
@@ -0,0 +1,57 @@
+<?php 
+
+  require "konexioa.php";
+  
+  session_start();
+  
+  header( 'X-Content-Type-Options: nosniff' );
+  header( 'X-Frame-Options: SAMEORIGIN' );
+  header( 'X-XSS-Protection: 1;mode=block' );
+  header_remove('X-Powered-By');
+  
+  $email= $_SESSION['Email'];
+
+  $izena=$_POST['Izena'];
+  $nan= $_POST['NAN'];
+  $telefonoa= $_POST['Telefonoa'];
+  $jaiotza= $_POST['Jaiotza'];
+  $pasahitza= $_POST['Pasahitza'];
+  $pasahitzaBerria= $_POST['PasahitzaBerria'];
+  
+  $stmt = $conn->prepare("SELECT * FROM erabiltzaileak WHERE email=?"); 
+  $stmt->bind_param("s", $email);
+  $stmt->execute(); 
+  $resEmail= $stmt->get_result(); 
+  $row = mysqli_fetch_array($resEmail);
+  
+  $hash_zaharra =$row['Pasahitza'] ;  #email horren hash pasahitza lortu
+  
+  if (password_verify($pasahitza, $hash_zaharra)) { #pasahitzak konparatu  
+	
+	$hash_berria= password_hash($pasahitzaBerria, PASSWORD_DEFAULT); #pasahitz berriaren hash-a kalkulatu
+	
+	
+	$stmt = $conn->prepare("UPDATE erabiltzaileak SET NAN = ?, Izen_abizenak = ?, Telefonoa = ?, Jaiotze_data = ?, Pasahitza = ? WHERE Email= ?"); 
+	$stmt->bind_param("ssisss", $nan,$izena, $telefonoa,$jaiotza, $hash_berria,$email);
+
+	if($stmt->execute()){
+		header("Location: http://localhost:81/erabiltzailearenMenua.php"); 
+		exit;
+	}
+  
+  }  
+  	
+  
+ 
+?>
+ <html lang="es">
+
+<header>
+<meta http-equiv="Content-Security-Policy" content="default-src 'none';">
+</header>
+
+</html>
+ 
+
+
+ 
---
+++ app/log.php
@@ -0,0 +1,95 @@
+<?php
+
+
+require "konexioa.php";
+
+
+session_start();
+
+
+
+header( 'X-Content-Type-Options: nosniff' );
+header( 'X-Frame-Options: SAMEORIGIN' );
+header( 'X-XSS-Protection: 1;mode=block' );
+header_remove('X-Powered-By'); 
+header("Set-Cookie: path=/;  HttpOnly; SameSite=Lax");   #PROBLEMASSS
+
+
+if (!empty($_POST['csrf']) && !empty($_SESSION['token'])) {         #PROBLEMASSS(app error disclousure OWASP)
+	if (hash_equals($_SESSION['token'], $_POST['csrf'])) {
+		// Procesar el formulario
+		$email= $_POST['Email'];
+		$pasahitza= $_POST['Pasahitza'];
+		$izena= $_POST['Izen abizenak'];
+		
+		
+		$stmt = $conn->prepare("SELECT * FROM erabiltzaileak WHERE email=?"); 
+		$stmt->bind_param("s", $email);
+		$stmt->execute(); 
+		$resEmail= $stmt->get_result(); 
+		$row = mysqli_fetch_array($resEmail);
+		
+		if($row['Email']!=null){ #erabiltzailea erregistratuta egotekotan, pasahitzak aztertu
+			$_SESSION['Email'] = $email;  
+			$_SESSION['Izen abizenak'] =$row['Izen_abizenak'];
+			
+			$hash_pasahitza =$row['Pasahitza'] ;  #email horren hash pasahitza lortu
+			
+			if (password_verify($pasahitza, $hash_pasahitza)) { #pasahitzak konparatu
+				if(!file_exists("./log")){
+                            		mkdir("./log", 755);
+                		}
+				date_default_timezone_set('Europe/Madrid');
+				error_log("127.0.0.1 $email [".date('r')."] 'Login ondo'".PHP_EOL,3,"./log/login.log");
+				header("Location: http://localhost:81/erabiltzailearenMenua.php");   
+				
+			}
+			else{ #pasahitza ez da egokia
+				if(!file_exists("./log")){
+                            		mkdir("./log", 755);
+                		}
+				date_default_timezone_set('Europe/Madrid');
+				error_log("127.0.0.1 $email [".date('r')."] 'Login txarto'".PHP_EOL,3,"./log/login.log");
+				header("Location: http://localhost:81/index.php?txarto=1");
+			}   
+		}
+		else { # email ez dago
+			if(!file_exists("./log")){
+                            mkdir("./log", 755);
+                	}
+			date_default_timezone_set('Europe/Madrid');
+			error_log("127.0.0.1 $email [".date('r')."] 'Login txarto'".PHP_EOL,3,"./log/login.log");
+			header("Location: http://localhost:81/index.php?txarto=1");
+		}
+  
+	}
+	else{ #token ezberdinak
+		if(!file_exists("./log")){
+                	mkdir("./log", 755);
+                }
+ 		date_default_timezone_set('Europe/Madrid');
+		error_log("127.0.0.1 $email [".date('r')."] 'Login txarto'".PHP_EOL,3,"./log/login.log");
+		header("Location: http://localhost:81/index.php?txarto=1");
+ 	}
+}
+else{ #token hutsa
+	if(!file_exists("./log")){
+        	mkdir("./log", 755);
+        }
+	date_default_timezone_set('Europe/Madrid');
+	error_log("127.0.0.1 $email [".date('r')."] 'Login txarto'".PHP_EOL,3,"./log/login.log");
+	header("Location: http://localhost:81/index.php?txarto=1");
+}
+    
+
+
+    
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<header>
+<meta http-equiv="Content-Security-Policy" content="default-src 'none';" >
+</header>
+</html>
